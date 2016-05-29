345a0bf8179990548edf019294ae683063f5dd8c
klxiao98 <klxiao98@gmail.com>
2016-05-28 23:19:58 -0700

    Update BackendSettings.java

--- app/src/main/java/com/eksell/eksell/utility/BackendSettings.java
+++ app/src/main/java/com/eksell/eksell/utility/BackendSettings.java
@@ -1,11 +1,30 @@
 package com.eksell.eksell.utility;
 
 /**
- * Provides values used to communicate with backend.
+ * Provides the values used to communicate with Backendless
+ * 
+ * @author Eileen Mao
+ * @version May 9, 2016
+ * 
+ * @author Period - 3
+ * @author Assignment - EKSell
+ * 
+ * @author Sources - Backendless API
  */
 public class BackendSettings
 {
-  public static final String APPLICATION_ID = "0FDC3841-82EF-6DCF-FFFE-52C18848C900";
-  public static final String SECRET_KEY = "77203AAF-3B5A-CAB6-FFFF-DCADB8586D00";
-  public static final String VERSION = "v1";
+    /**
+     * EKSell Application ID for Backendless
+     */
+    public static final String APPLICATION_ID = "0FDC3841-82EF-6DCF-FFFE-52C18848C900";
+    
+    /**
+     * Key for the EKSell Application for Backendless
+     */
+    public static final String SECRET_KEY = "77203AAF-3B5A-CAB6-FFFF-DCADB8586D00";
+    
+    /**
+     * Version of Backendless used
+     */
+    public static final String VERSION = "v1";
 }
