5e77f6c5a9faa474ded6657b4caa6c58e76efb59
klxiao98 <klxiao98@gmail.com>
2016-05-28 23:12:43 -0700

    Update DialogHelper.java

--- app/src/main/java/com/eksell/eksell/utility/DialogHelper.java
+++ app/src/main/java/com/eksell/eksell/utility/DialogHelper.java
@@ -4,10 +4,25 @@ import android.app.AlertDialog;
 import android.content.Context;
 
 /**
- * Created by emao on 5/24/16.
+ * Class for creating the error message for the User Interface
+ * 
+ * @author Eileen Mao
+ * @version May 10, 2016
+ * 
+ * @author Period - 3
+ * @author Assignment - EKSell
+ * 
+ * @author Sources - Android Developer API
  */
 public class DialogHelper {
 
+    /**
+     * Creates the error message
+     * @param context the context of the object
+     * @param title the title of the error
+     * @param message the message of the error
+     * @return an alert of the error
+     */
     public static AlertDialog createErrorDialog(Context context, String title, String message )
     {
         return new AlertDialog.Builder( context )
