6ef33174aa5df084de78784603b83ca22fac2ef1
klxiao98 <klxiao98@gmail.com>
2016-05-28 23:08:26 -0700

    Update Validator.java

--- app/src/main/java/com/eksell/eksell/utility/Validator.java
+++ app/src/main/java/com/eksell/eksell/utility/Validator.java
@@ -5,9 +5,25 @@ import android.text.TextUtils;
 import android.util.Patterns;
 import android.widget.Toast;
 import com.eksell.eksell.eksell.R;
-
+/**
+ * A Supporting class with static methods to help the LogIn and Register Activities to verify if the entries are valid
+ * 
+ * @author Eileen Mao
+ * @version May 10, 2016
+ * 
+ * @author Period - 3
+ * @author Assignment - EKSell
+ * 
+ * @author Sources - None
+ */
 public class Validator
 {
+  /**
+   * Checks to see if the name is valid (not empty)
+   * @param currentContext is the current state of the object
+   * @param name is the entered name
+   * @return true if name is valid, false otherwise (and displays a Toast)
+     */
   public static boolean isNameValid( Context currentContext, CharSequence name )
   {
     if( name.toString().isEmpty() )
@@ -18,7 +34,13 @@ public class Validator
 
     return true;
   }
-
+  /**
+   * Checks to see if the email is valid (not empty and follows email format)
+   * @param currentContext is the current state of the object
+   * @param email is the entered email
+   * @return true if email is valid, false otherwise (and displays a Toast based on the type of
+   * error
+   */
   public static boolean isEmailValid( Context currentContext, CharSequence email )
   {
     if( email.toString().isEmpty() )
@@ -36,6 +58,12 @@ public class Validator
     return true;
   }
 
+  /**
+   * Checks to see if the password is valid (not empty)
+   * @param currentContext is the current state of the object
+   * @param password is the entered password
+   * @return if password is valid
+   */
   public static boolean isPasswordValid( Context currentContext, CharSequence password )
   {
     if( password.toString().isEmpty() )
@@ -46,7 +74,12 @@ public class Validator
 
     return true;
   }
-
+  /**
+     * Checks to see if the password matches the confirmation password
+     * @param password is the password entered
+     * @param passwordConfirm is the confirmation of the password
+     * @return true if the passwords match, false otherwise
+     */
   public static boolean isPasswordsMatch( Context currentContext, CharSequence password, CharSequence passwordConfirm )
   {
     if( !TextUtils.equals( password, passwordConfirm ) )
