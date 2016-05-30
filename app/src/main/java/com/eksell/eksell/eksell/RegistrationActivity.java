3fa832c16cdfcdcff73d9e4744142dd71c26ffa9
emaoo <mao.eileen@gmail.com>
2016-05-29 21:27:53 -0700

    fix registration

--- app/src/main/java/com/eksell/eksell/eksell/RegistrationActivity.java
+++ app/src/main/java/com/eksell/eksell/eksell/RegistrationActivity.java
@@ -3,12 +3,15 @@ package com.eksell.eksell.eksell;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.backendless.Backendless;
 import com.backendless.BackendlessUser;
+import com.backendless.async.callback.AsyncCallback;
 import com.eksell.eksell.utility.BackendSettings;
 import com.eksell.eksell.utility.LoadingCallback;
 import com.eksell.eksell.utility.Validator;
@@ -42,34 +45,38 @@ public class RegistrationActivity extends AppCompatActivity {
 
         // click registration button
         Button registerButton = (Button) findViewById( R.id.registerButton );
-        registerButton.setOnClickListener(new View.OnClickListener() {
-            
-            // once clicked, retrieves the entries and saves it into a user onto Backendless
-            @Override
-            public void onClick(View v) {
-                EditText nameField = (EditText) findViewById( R.id.nameField );
-                EditText emailField = (EditText) findViewById( R.id.emailField );
-                EditText passwordField = (EditText) findViewById( R.id.passwordField );
-                EditText passwordConfirmField = (EditText) findViewById( R.id.passwordConfirmField );
 
-                CharSequence name = nameField.getText();
-                CharSequence email = emailField.getText();
-                CharSequence password = passwordField.getText();
-                CharSequence passwordConfirmation = passwordConfirmField.getText();
+        View.OnClickListener registerButtonClickListener = createRegisterButtonClickListener();
 
-                if( isRegistrationValuesValid( name, email, password, passwordConfirmation ) )
-                {
-                    BackendlessUser user = new BackendlessUser();
-                    user.setEmail( email.toString() );
-                    user.setPassword( password.toString() );
-                    user.setProperty( "name", name );
+        registerButton.setOnClickListener( registerButtonClickListener );
+    }
 
-                    LoadingCallback<BackendlessUser> registrationCallback = createRegistrationCallback();
-                    registrationCallback.showLoading();
-                    Backendless.UserService.register( user, registrationCallback );
-                }
-            }
-        });
+    /**
+     * Checks to see if the entered values are valid
+     * @param name is the name of the user
+     * @param email is the user's email
+     * @param password is the user's desired password
+     * @param passwordConfirm is a confirmation of the password
+     * @return true if the registration is valid, false otherwise
+     */
+    public boolean isRegistrationValuesValid( CharSequence name, CharSequence email, CharSequence password,
+                                              CharSequence passwordConfirm )
+    {
+        return Validator.isNameValid( this, name )
+                && Validator.isEmailValid( this, email )
+                && Validator.isPasswordValid( this, password )
+                && Validator.isPasswordsMatch( this, password, passwordConfirm );
+    }
+
+    public void registerUser( String name, String email, String password,
+                              AsyncCallback<BackendlessUser> registrationCallback )
+    {
+        BackendlessUser user = new BackendlessUser();
+        user.setEmail( email );
+        user.setPassword( password );
+        user.setProperty( "name", name );
+
+        Backendless.UserService.register( user, registrationCallback );
     }
 
     /**
@@ -96,20 +103,32 @@ public class RegistrationActivity extends AppCompatActivity {
             }
         };
     }
-    /**
-     * Checks to see if the entered values are valid
-     * @param name is the name of the user
-     * @param email is the user's email
-     * @param password is the user's desired password
-     * @param passwordConfirm is a confirmation of the password
-     * @return true if the registration is valid, false otherwise
-     */
-    private boolean isRegistrationValuesValid( CharSequence name, CharSequence email,
-                                              CharSequence password, CharSequence passwordConfirm )
+
+    public View.OnClickListener createRegisterButtonClickListener()
     {
-        return Validator.isNameValid( this, name )
-                && Validator.isEmailValid( this, email )
-                && Validator.isPasswordValid( this, password )
-                && Validator.isPasswordsMatch( this, password, passwordConfirm );
+        return new View.OnClickListener()
+        {
+            @Override
+            public void onClick( View v )
+            {
+                EditText nameField = (EditText) findViewById( R.id.nameField );
+                EditText emailField = (EditText) findViewById( R.id.emailField );
+                EditText passwordField = (EditText) findViewById( R.id.passwordField );
+                EditText passwordConfirmField = (EditText) findViewById( R.id.passwordConfirmField );
+
+                CharSequence name = nameField.getText();
+                CharSequence email = emailField.getText();
+                CharSequence password = passwordField.getText();
+                CharSequence passwordConfirmation = passwordConfirmField.getText();
+
+                if( isRegistrationValuesValid( name, email, password, passwordConfirmation ) )
+                {
+                    LoadingCallback<BackendlessUser> registrationCallback = createRegistrationCallback();
+
+                    registrationCallback.showLoading();
+                    registerUser( name.toString(), email.toString(), password.toString(), registrationCallback );
+                }
+            }
+        };
     }
 }
