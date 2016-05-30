7ab715e38dd8d75c55d3fe5ee7db022e1f0be68d
klxiao98 <klxiao98@gmail.com>
2016-05-30 11:32:42 -0700

    documentation

--- app/src/main/java/com/eksell/eksell/entities/Future.java
+++ app/src/main/java/com/eksell/eksell/entities/Future.java
@@ -5,15 +5,39 @@ import com.backendless.exceptions.BackendlessException;
 import com.backendless.exceptions.BackendlessFault;
 
 /**
- * Created by emao on 5/24/16.
+ * A class required by Backendless to work things in the background and display errors
+ *
+ * @author Sergey Chupov (Backendless admin), Documentatio by Katherine Xiao
+ * @version February 25, 2015
+ *
  */
 public class Future<T> implements AsyncCallback<T>
 {
+    /**
+     * the result
+     */
     private T result;
+    
+    /**
+     * runtime exception
+     */
     private RuntimeException exception;
-    private boolean complete;
-    private boolean fault;
 
+     /**
+     * if the run is complete
+     */
+    private boolean complete;
+    
+    /**
+     * if fault exists
+     */
+    private boolean fault;
+    
+    /**
+     * waits for completion, and if there is an error, throws the exception
+     * @return result
+     * @throws InterruptedException if the process is interrupted
+     */
     public synchronized T get() throws InterruptedException
     {
         while( !complete )
@@ -28,14 +52,22 @@ public class Future<T> implements AsyncCallback<T>
 
         return result;
     }
-
+    
+    /**
+     * sets the results is successful
+     * @param t the response
+     */
     public synchronized void set( T t )
     {
         result = t;
         complete = true;
         notifyAll();
     }
-
+    
+    /**
+     * response to an error
+     * @param e the runtime exception
+     */
     public synchronized void fault( RuntimeException e )
     {
         exception = e;
@@ -43,23 +75,39 @@ public class Future<T> implements AsyncCallback<T>
         complete = true;
         notifyAll();
     }
-
+    
+    /**
+     * if the process is complete
+     * @return if the process is complete
+     */
     public boolean isComplete()
     {
         return complete;
     }
-
+    
+    /**
+     * if there is an error
+     * @return if there is an error
+     */
     public boolean isFault()
     {
         return fault;
     }
-
+    
+    /**
+     * handles the response if it is successful
+     * @param response is the response retrieved by the server
+     */
     @Override
     public void handleResponse( T response )
     {
         set( response );
     }
-
+    
+    /**
+     * handles the fault
+     * @param fault the fault retrieved by the server
+     */
     @Override
     public void handleFault( BackendlessFault fault )
     {
