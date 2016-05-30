d33d1357c090b606d7400dc7983d8eb09dde839a
klxiao98 <klxiao98@gmail.com>
2016-05-30 14:32:34 -0700

    added some documentation

--- app/src/main/java/com/eksell/eksell/adapters/ItemAdaptor.java
+++ app/src/main/java/com/eksell/eksell/adapters/ItemAdaptor.java
@@ -19,20 +19,56 @@ import java.text.DecimalFormat;
 import java.util.List;
 
 /**
- * Created by emao on 5/20/16.
+ * An adaptor that converts the item to a viewable layout
+ *
+ * @author Eileen Mao, Katherine Xiao, Sean Meng
+ * @version May 26, 2016
+ * 
+ * @author Period - 3
+ * @author Assignment - EKSell
+ *
+ * @Sources Android API, StackOverflow
  */
 public class ItemAdaptor extends ArrayAdapter<Item> {
+    /**
+     * Inflator that instantiates the layout XML file into its View
+     */
     private LayoutInflater mInflater;
+    
+    /**
+     * The resource ID for a layout file containing a TextView to use when instantiating views.
+     */
     private int mResource;
+    
+    /**
+     * item instance
+     */
     Item item;
-
+    
+    /**
+     * ItemAdaptor constructor
+     * @param context is the current context
+     * @param resource is the resource ID for a layout file containing a TextView
+     *                 to use when instantiating views
+     * @param restaurants is the objects to represent in the ListView
+     */
     public ItemAdaptor(Context context, int resource, List<Item> restaurants )
     {
         super( context, resource, restaurants );
         mResource = resource;
         mInflater = (LayoutInflater) context.getSystemService( Context.LAYOUT_INFLATER_SERVICE );
     }
-
+    
+    /**
+     * Converts the item from their respective instances and pictures into a presentable layout.
+     * Also, when the buyer clicks the "BUY" button, directs the buyer to the Order Activity where
+     * he or she  can email the seller demonstrating interest
+     * @param position is the position of the item within the adapter's data set of the item whose
+     *                 view we want
+     * @param convertView is the old view to reuse
+     * @param parent is the parent that this view will eventually be attached to
+     * @return the view that displays the data at the specified position in the data set
+     */
     @Override
     public View getView(int position, View convertView, ViewGroup parent )
     {
@@ -41,7 +77,6 @@ public class ItemAdaptor extends ArrayAdapter<Item> {
         // get the item data from Adaptor
         item = this.getItem( position );
 
-        // populate the view
         TextView itemNameView = (TextView) view.findViewById( R.id.itemName);
         TextView descriptionView = (TextView) view.findViewById( R.id.itemDescription);
         TextView itemPriceView = (TextView) view.findViewById( R.id.itemPrice);
@@ -53,7 +88,7 @@ public class ItemAdaptor extends ArrayAdapter<Item> {
         itemPriceView.setText( "$" + df.format(item.getPrice() / 100.0) );
 
         if ((item.getImageUrl() != null) && (!item.getImageUrl().trim().isEmpty())) {
-            // Use google Picasso library to populate the image from Url
+            // Use google Picasso library to retrieve the image from Url
             Picasso.with(this.getContext()).load(item.getImageUrl()).into(ivItemImage);
         } else {
             ivItemImage.setImageResource(R.drawable.sample_image);
@@ -62,6 +97,11 @@ public class ItemAdaptor extends ArrayAdapter<Item> {
         // Click Buy it Now Button
         ImageView btBuyNow = (ImageView) view.findViewById( R.id.itemBuyNow);
         btBuyNow.setOnClickListener(new View.OnClickListener() {
+            /**
+             * When the user clicks on the "BUY" button, a message is displayed to fill in
+             * the fields and is subsequently directed to the Order Activity.
+             * @param v is the current view
+             */
             @Override
             public void onClick(View v) {
                 Toast.makeText(getContext(),
