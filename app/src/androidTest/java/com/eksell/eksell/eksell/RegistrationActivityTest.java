7247803939edbc66fe816941aac151c407b4fdad
klxiao98 <klxiao98@gmail.com>
2016-05-30 17:11:40 -0700

    Update RegistrationActivityTest.java

--- app/src/androidTest/java/com/eksell/eksell/eksell/RegistrationActivityTest.java
+++ app/src/androidTest/java/com/eksell/eksell/eksell/RegistrationActivityTest.java
@@ -6,25 +6,56 @@ import android.widget.EditText;
 import com.robotium.solo.Solo;
 
 /**
- * Created by emao on 5/24/16.
+ * Instrumentation Testing for the Registration Activity class
  */
 public class RegistrationActivityTest extends ActivityInstrumentationTestCase2<RegistrationActivity> {
+    /**
+     * Mock user
+     */
     private Solo solo;
-
+    /**
+     * editable name field
+     */
     private EditText nameField;
+    /**
+     * editable email field
+     */
     private EditText emailField;
+    /**
+     * editable password field
+     */
     private EditText passwordField;
+
+    /**
+     * editable confirm password field
+     */
     private EditText passwordConfirmField;
 
+    /**
+     * test valid name
+     */
     private String validName = "Eileen Mao";
+    /**
+     * test valid email
+     */
     private String validEmail = "emao@gmail.com";
+    /**
+     * test valid password
+     */
     private String validPassword = "myPassw0rd";
 
+    /**
+     * Constructor
+     */
     public RegistrationActivityTest()
     {
         super( RegistrationActivity.class );
     }
 
+    /**
+     * sets up the information retrieved
+     * @throws Exception if error occurs
+     */
     public void setUp() throws Exception
     {
         solo = new Solo( getInstrumentation(), getActivity() );
@@ -35,6 +66,9 @@ public class RegistrationActivityTest extends ActivityInstrumentationTestCase2<R
         passwordConfirmField = (EditText) solo.getView( R.id.passwordConfirmField );
     }
 
+    /**
+     * tests if there is an empty name (aka nothing entered in name field)
+     */
     public void testEmptyNameValidation()
     {
         solo.enterText( nameField, "" );
@@ -42,12 +76,15 @@ public class RegistrationActivityTest extends ActivityInstrumentationTestCase2<R
         solo.enterText( passwordField, validPassword );
         solo.enterText( passwordConfirmField, validPassword );
 
-        solo.clickOnButton( solo.getString( R.string.button_register ) );
+        solo.clickOnButton( "Register" );
 
         assertTrue( "Warning in Toast is missing or invalid", solo.waitForText(
                 solo.getString( R.string.warning_name_empty ) ) );
     }
 
+    /**
+     * tests if there is an empty email (aka nothing entered in email field)
+     */
     public void testEmptyEmail()
     {
         solo.enterText( nameField, validName );
@@ -55,12 +92,15 @@ public class RegistrationActivityTest extends ActivityInstrumentationTestCase2<R
         solo.enterText( passwordField, validPassword );
         solo.enterText( passwordConfirmField, validPassword );
 
-        solo.clickOnButton( solo.getString( R.string.button_register ) );
+        solo.clickOnButton( "Register" );
 
         assertTrue( "Warning in Toast is missing or invalid", solo.waitForText(
                 solo.getString( R.string.warning_email_empty ) ) );
     }
 
+    /**
+     * tests if there is an invalid email (aka does not follow email format)
+     */
     public void testInvalidEmail()
     {
         solo.enterText( nameField, validName );
@@ -68,12 +108,15 @@ public class RegistrationActivityTest extends ActivityInstrumentationTestCase2<R
         solo.enterText( passwordField, validPassword );
         solo.enterText( passwordConfirmField, validPassword );
 
-        solo.clickOnButton( solo.getString( R.string.button_register ) );
+        solo.clickOnButton( "Register");
 
         assertTrue( "Warning in Toast is missing or invalid", solo.waitForText(
                 solo.getString( R.string.warning_email_invalid ) ) );
     }
 
+    /**
+     * tests if there is an empty password (aka nothing entered in password field)
+     */
     public void testEmptyPassword()
     {
         solo.enterText( nameField, validName );
@@ -81,12 +124,15 @@ public class RegistrationActivityTest extends ActivityInstrumentationTestCase2<R
         solo.enterText( passwordField, "" );
         solo.enterText( passwordConfirmField, validPassword );
 
-        solo.clickOnButton( solo.getString( R.string.button_register ) );
+        solo.clickOnButton( "Register" );
 
         assertTrue( "Warning in Toast is missing or invalid", solo.waitForText(
                 solo.getString( R.string.warning_password_empty ) ) );
     }
 
+    /**
+     * tets if the password and confirmation password do not match
+     */
     public void testNotMatchingPasswords()
     {
         solo.enterText( nameField, validName );
@@ -94,12 +140,16 @@ public class RegistrationActivityTest extends ActivityInstrumentationTestCase2<R
         solo.enterText( passwordField, validPassword );
         solo.enterText( passwordConfirmField, passwordConfirmField+"1" );
 
-        solo.clickOnButton( solo.getString( R.string.button_register ) );
+        solo.clickOnButton( "Register" );
 
         assertTrue( "Warning in Toast is missing or invalid", solo.waitForText(
                 solo.getString( R.string.warning_passwords_do_not_match ) ) );
     }
 
+    /**
+     * exits the activity
+     * @throws Exception if an error occurs
+     */
     @Override
     public void tearDown() throws Exception
     {
